5175bef964f3723f516c5ee28d2a6c1e713a2d99
Javier Vera <javier.vera@clarite.pl>
2024-04-19 11:34:24 +0200

    drobnostka

---
+++ data-types-and-operators/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int c = a + b;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("result: " + c);
+
+        double d = 2;
+        double e = 3;
+
+        double f = d / e;
+
+        System.out.println(f);
+    }
+}
+
